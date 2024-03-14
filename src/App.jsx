a97b144b08dce87d5c313a9c3e419d423c9a06b4
anomalous254 <anomalous254@gmail.com>
2024-03-14 14:15:42 +0300

    fix: fixed not rendering elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import "./App.scss";
+import React from 'react';
+import './App.scss';
+import {
+    createBrowserRouter,
+    RouterProvider,
+    createRoutesFromElements,
+    Route,
+} from 'react-router-dom';
+import Home from './pages/Home';
+
+const routes = createBrowserRouter(
+    createRoutesFromElements(<Route path="/" element={<Home />} />)
+);
 
 function App() {
-  return <h1>Lulu Game</h1>;
+    return <RouterProvider router={routes} />;
 }
 
-export default App;
+export default App
