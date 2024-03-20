4b46b52b3a1eaa2d64ac98cb84466a0e90be2be4
anomalous254 <anomalous254@gmail.com>
2024-03-20 14:46:12 +0300

    added avatars

---
+++ src/hooks/useAvatar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import av1 from '../assets/avatar/av1.png';
+import av2 from '../assets/avatar/av2.png';
+import av3 from '../assets/avatar/av3.png';
+import av4 from '../assets/avatar/av4.png';
+import av5 from '../assets/avatar/av5.png';
+import av6 from '../assets/avatar/av6.png';
+
+const avatars = [av1, av2, av3, av4, av5, av6];
+
+export const useAvatar = () => {
+    return avatars;
+};
--- src/pages/startgane/index.jsx
+++ src/pages/startgane/index.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import './index.scss';
+import { useAvatar } from '../../hooks';
 
 const languges = ['Yoruba', 'Swahili', 'Hausa', 'Igbo'];
-const avater = [
-    'Avatar-1',
-    'Avatar-2',
-    'Avatar-3',
-    'Avatar-4',
-    'Avatar-5',
-    'Avatar-6',
-];
-
 export const StartGame = () => {
+    const avatar = useAvatar();
     const [selectedLng, setSelectedLng] = React.useState('');
     const [selectedAvatar, setSelectedAvatar] = React.useState('');
     const [activeButton, setActiveButton] = React.useState(null);
@@ -44,24 +37,33 @@ export const StartGame = () => {
             <div className="select-avatar">
                 <h4>Select avatar</h4>
                 <div className="avatar-btn">
-                    {avater.map((icon) => (
+                    {avatar.map((icon) => (
                         <button
                             onClick={() => setSelectedAvatar(icon)}
                             key={icon}
                         >
-                            {icon}
+                            <img src={icon} alt="avatar" />
                         </button>
                     ))}
                 </div>
             </div>
 
-            <div className="selected">
-                <h4>Your Selection</h4>
-                <div>
-                    <h5>Language: {selectedLng}</h5>
-                    <h5>Avatar: {selectedAvatar}</h5>
+            {selectedLng ? (
+                <div className="selected">
+                    <h4>Your Selection</h4>
+                    <div className="my-selection">
+                        <img src={selectedAvatar} alt="avatar" />
+                        <h5>
+                            <span>{selectedLng}</span>
+                        </h5>
+                    </div>
+                    <div className="start-btn">
+                        {selectedAvatar && selectedLng ? (
+                            <button>Start Game</button>
+                        ) : null}
+                    </div>
                 </div>
-            </div>
+            ) : null}
         </div>
     );
 };
