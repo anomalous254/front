cac0c9f63528154479ce4a9eb17457edaf8644be
anomalous254 <anomalous254@gmail.com>
2024-03-22 13:57:59 +0300

    add footer to menu sm screens

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -7,10 +7,13 @@ import { Link } from 'react-router-dom';
 import { RiMenu3Fill } from 'react-icons/ri';
 import { IoMdCloseCircleOutline } from 'react-icons/io';
 import { NavLink } from 'react-router-dom';
+import { FaBookOpen } from 'react-icons/fa6';
+import { FaInfo } from 'react-icons/fa';
+import { IoGitBranch } from 'react-icons/io5';
 
 export const Header = () => {
     const [showMenu, setShowMenu] = React.useState(false);
-    
+
     return (
         <React.Fragment>
             <header className="header">
@@ -36,43 +39,68 @@ export const Header = () => {
                     </button>
                 </div>
 
-                <div
-                    className={
-                        showMenu
-                            ? 'show-menu active '
-                            : `show-menu`
-                    }
-                >
-                    <div className="menu-sm-screens">
-                        <NavLink onClick={() => setShowMenu(!showMenu)} to="/">
-                            Start
-                        </NavLink>
-                        <NavLink
-                            onClick={() => setShowMenu(!showMenu)}
-                            to="words"
+                {showMenu ? (
+                    <div className="menu-container">
+                        <div
+                            className={
+                                showMenu ? 'show-menu active ' : `show-menu`
+                            }
                         >
-                            Words
-                        </NavLink>
-                        <NavLink
-                            onClick={() => setShowMenu(!showMenu)}
-                            to="reviews"
-                        >
-                            Tribe reviews
-                        </NavLink>
-                        <NavLink
-                            onClick={() => setShowMenu(!showMenu)}
-                            to="leaderboard"
-                        >
-                            Leaderboard
-                        </NavLink>
-                        <NavLink
-                            onClick={() => setShowMenu(!showMenu)}
-                            to="/login"
-                        >
-                            Quit
-                        </NavLink>
+                            <div className="menu-sm-screens">
+                                <NavLink
+                                    onClick={() => setShowMenu(!showMenu)}
+                                    to="/"
+                                >
+                                    Start
+                                </NavLink>
+                                <NavLink
+                                    onClick={() => setShowMenu(!showMenu)}
+                                    to="words"
+                                >
+                                    Words
+                                </NavLink>
+                                <NavLink
+                                    onClick={() => setShowMenu(!showMenu)}
+                                    to="reviews"
+                                >
+                                    Tribe reviews
+                                </NavLink>
+                                <NavLink
+                                    onClick={() => setShowMenu(!showMenu)}
+                                    to="leaderboard"
+                                >
+                                    Leaderboard
+                                </NavLink>
+                                <NavLink
+                                    onClick={() => setShowMenu(!showMenu)}
+                                    to="/login"
+                                >
+                                    Quit
+                                </NavLink>
+                                <div className="footer-sm-screen">
+                                    <div className="footer-sm-link">
+                                        <Link>
+                                            <FaBookOpen className="footer-sm-icon" />
+                                        </Link>
+                                        <p>Privacy policy</p>
+                                    </div>
+                                    <div className="footer-sm-link">
+                                        <Link>
+                                            <IoGitBranch className="footer-sm-icon" />
+                                        </Link>
+                                        <p>version 1.0.1</p>
+                                    </div>
+                                    <div className="footer-sm-link">
+                                        <Link>
+                                            <FaInfo className="footer-sm-icon" />
+                                        </Link>
+                                        <p>Read more</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                ) : null}
             </header>
         </React.Fragment>
     );
