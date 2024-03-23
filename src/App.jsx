1476ab6dba43d5d62c72a4e21ff3fad288cfd4c6
anomalous254 <anomalous254@gmail.com>
2024-03-23 14:41:57 +0300

    add  quit prompt

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,19 @@ import {
 } from 'react-router-dom';
 import { DashboardLayout } from './layouts';
 import { StartGame, Game, ReviewList } from './pages';
+import { LoadingPage } from './components';
 
 const routes = createBrowserRouter(
     createRoutesFromElements(
-        <Route path="/" element={<DashboardLayout />}>
-            <Route index element={<StartGame />} />
-            <Route path="game" element={<Game />} />
-            <Route path="reviews" element={<ReviewList />} />
-            <Route path="*" element={<h3>Page Not Found</h3>} />
-        </Route>
+        <>
+            <Route path="/" element={<LoadingPage />}></Route>
+            <Route path="dashboard" element={<DashboardLayout />}>
+                <Route index element={<StartGame />} />
+                <Route path="game" element={<Game />} />
+                <Route path="reviews" element={<ReviewList />} />
+                <Route path="*" element={<h3>Page Not Found</h3>} />
+            </Route>
+        </>
     )
 );
 
--- src/components/loadingPage/index.jsx
+++ src/components/loadingPage/index.jsx
@@ -3,18 +3,38 @@ import './index.scss';
 import bot from '../../assets/avatar/bot.png';
 import logo from '../../assets/img/lulu-white-logo.png';
 import { Link } from 'react-router-dom';
+import { GoIssueClosed } from 'react-icons/go';
+import { IoIosCloseCircleOutline } from 'react-icons/io';
 
 export const LoadingPage = () => {
+    const [quit, setQuit] = React.useState(false);
     return (
         <div className="splash-screen">
             <img src={bot} alt="logo" />
             <div className="left-section">
-                <Link>Start Game</Link>
-                <Link>Words/Phrases</Link>
-                <Link>Tribe Reviews</Link>
-                <Link>Leadership Board</Link>
-                <Link to="https://lulugame.vercel.app/">Quit</Link>
+                <Link to="/dashboard">Start Game</Link>
+                <Link to="/dashboard/words">Words/Phrases</Link>
+                <Link to="/dashboard/reviews">Tribe Reviews</Link>
+                <Link to="/dashboard/leadership">Leadership Board</Link>
+                <Link onClick={() => setQuit(true)}>Quit</Link>
             </div>
+
+            {quit ? (
+                <div className="quit">
+                    <div className="prompt">
+                        <img src={bot} alt="logo" className="quit-bot" />
+                        <p>Do you want to Qiut this game ?</p>
+                        <div className="quit-btn">
+                            <Link to="https://lulugame.vercel.app/">
+                                <GoIssueClosed className="quit-link" />
+                            </Link>
+                            <button onClick={() => setQuit(!quit)}>
+                                <IoIosCloseCircleOutline className="close-prompt" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            ) : null}
         </div>
     );
 };
